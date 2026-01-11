ecd2b7d56179b885d2577a97742e240aacd537c2
Ana <analu189266@gmail.com>
2026-01-11 12:12:27 -0300

    organizando

---
+++ estudos-linguagens/desafios-js/d004/ex001.js
@@ -0,0 +1,13 @@
+
+let idade2 = 3
+
+if(idade2 < 15 && idade2 >= 12){
+    console.log("você está no ensino fundamental!")
+} else if(idade2 <= 11){
+    console.log("você está no ensino básico!")
+} else if(idade2 > 12 && idade2 <= 18){
+    console.log("você está no ensino médio")
+} else {
+    console.log("você já saiu da escola, está cursando o superior?")
+}
+

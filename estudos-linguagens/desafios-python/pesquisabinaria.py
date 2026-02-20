f2ead3dbd1ca2688da520b75a1a8ed4a0452755a
Ana Luiza <analu189266@gmail.com>
2026-02-20 19:16:34 -0300

    feat: nova pasta pyhton/ex pesquisa binaria

---
+++ estudos-linguagens/desafios-python/pesquisabinaria.py
@@ -0,0 +1,26 @@
+# livro: entendendo algoritmos.
+
+def pesquisa_binaria(lista, item): 
+    # inicializa as variveis da lista/chutes
+    baixo = 0
+    alto = (len(lista) -1)
+    # enquanto ainda não achar um único elemento, ele continua verificando o elemento central.
+    while baixo <= alto:
+        # necessário o int para ele arredondar os numeros, não aceita float.
+        meio = int((baixo + alto) / 2)
+        chute = lista[meio]
+        # acha o item
+        if chute == item: 
+            return meio
+        # chute muito alto
+        if chute > item:
+            alto = meio - 1
+        # chute muito baixo
+        else:
+            baixo = meio + 1
+    return None
+
+minha_lista = [1, 3, 5, 7, 9]
+
+print(pesquisa_binaria(minha_lista, 3))
+print(pesquisa_binaria(minha_lista, -1))

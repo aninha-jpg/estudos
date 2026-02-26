804ee9d0f8316c52241359841d0de096f9c963eb
Ana Luiza <185213825+aninha-jpg@users.noreply.github.com>
2026-02-26 20:31:48 -0300

    fix: remove (r) from error

--- estudos-linguagens/estudo-apis/api-cep/script.js
+++ estudos-linguagens/estudo-apis/api-cep/script.js
@@ -18,7 +18,7 @@ async function foundCEP() {
         const response = await fetch(`https://viacep.com.br/ws/${cleanCEP}/json/`);
         const data = await response.json();
         // se der algum erro ele acusa, se não ele escreve na tela as informações
-        if (data.error){
+        if (data.erro){
             result.innerHTML = `CEP não encontrado.`
         } else {
             result.innerHTML = `

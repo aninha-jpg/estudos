0b289c627a2b87d3cbf7f4f0f2bdd00b56e5f105
Ana Luiza <185213825+aninha-jpg@users.noreply.github.com>
2026-03-03 20:49:58 -0300

    feat: solve two sum problem with dictionary optimization

---
+++ estudos-linguagens/desafios-python/somando-alvos/somando-alvos.py
@@ -0,0 +1,19 @@
+Numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+target = 24
+
+#função O(n)
+
+def findTarget(num, target):
+    memories = {}
+    #guarda numeros já escolhidos na memória
+    for i, num in enumerate(Numbers):
+        complement = target - num
+        if complement in memories:
+            return [memories[complement], i]
+        
+        memories[num] = i
+        print(memories)
+    return "Nenhum par encontrado"
+
+print(findTarget(Numbers, target))
